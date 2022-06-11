47a6286c88f2e738d1e4fed478fe98afa9482f8a
Charilaos Giannopoulos <webdevcharilaosg@gmail.com>
2022-06-11 17:36:11 +0300

    added <info> button with transition and some js.

---
+++ script.js
@@ -0,0 +1,10 @@
+var tooltip = document.querySelector('#about-page')
+
+tooltip.addEventListener('click', function() {
+  if (this.classList.contains('active')) {
+    this.classList.remove('active');
+  } else {
+    this.classList.add('active');
+  }
+  
+});
