9753d5bef48f7af43ae6a86933520a842af694f4
Charilaos Giannopoulos <webdevcharilaosg@gmail.com>
2022-06-13 16:22:57 +0300

    added JS to study material

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function showText() {
         txtArea.classList.remove('active')
     }
 }
+
+linkSection = document.querySelectorAll('.ls').forEach(item => {
+    item.addEventListener('click', event => {
+        if(event.target.querySelector('ul').style.display == '' || event.target.querySelector('ul').style.display == 'none'){
+            event.target.querySelector('ul').style.display = 'block'
+            event.target.querySelector('i').classList.remove('fa-circle-plus')
+            event.target.querySelector('i').classList.add('fa-circle-minus')
+        } else {
+            event.target.querySelector('ul').style.display = 'none'
+            event.target.querySelector('i').classList.remove('fa-circle-minus')
+            event.target.querySelector('i').classList.add('fa-circle-plus')
+        }
+    })
+})
