00b9d6ba7aa918597a69706a528595c56cb91081
Charilaos Giannopoulos <webdevcharilaosg@gmail.com>
2022-07-22 21:07:18 +0300

    Main page rework done

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var tooltip = document.querySelector('#btn')
-var txtArea = document.querySelector('#ap-text')
-btn = document.querySelector('button')
+// var tooltip = document.querySelector('#btn')
+// var txtArea = document.querySelector('#ap-text')
+// btn = document.querySelector('button')
 
-function showText() {
-    if(txtArea.classList.contains('remove')){
-        txtArea.classList.add('active')
-        txtArea.classList.remove('remove')
-    } else {
-        txtArea.classList.add('remove')
-        txtArea.classList.remove('active')
-    }
-}
+// function showText() {
+//     if(txtArea.classList.contains('remove')){
+//         txtArea.classList.add('active')
+//         txtArea.classList.remove('remove')
+//     } else {
+//         txtArea.classList.add('remove')
+//         txtArea.classList.remove('active')
+//     }
+// }
 
 listButtons = document.querySelectorAll('.ls button').forEach(item => {
     item.onclick = ( event =>{
@@ -38,3 +38,13 @@ function showSubMenu(){
         document.querySelector('#submenu i').classList.add('color')
     }
 }
+
+function tooltips(){
+	let tooltips = document.querySelectorAll('.tt')
+
+	tooltips.forEach( t => {
+		new bootstrap.Tooltip(t)
+	})
+}
+
+tooltips()
